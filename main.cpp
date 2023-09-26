3fbed90fa015bd3fc2dd8bce49c68e7b62db1359
Kevin Jordil <kevin.jordil@outlook.com>
2023-09-26 17:04:07 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,41 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+#include "mbed_trace.h"
+#if defined(MBED_CONF_MBED_TRACE_ENABLE)
+#define TRACE_GROUP "MAIN"
+#endif // MBED_CONF_MBED_TRACE_ENABLE
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE 500ms
+
+#if !MBED_TEST_MODE
+int main()
+{
+#if defined(MBED_CONF_MBED_TRACE_ENABLE)
+    mbed_trace_init();
+#endif
+    // Initialise the digital pin LED1 as an output
+#ifdef LED1
+    DigitalOut led(LED1);
+#else
+    bool led;
+#endif
+
+    printf("Standard: Hello World!\n");
+    tr_debug("Debug: Hello World!");
+    tr_info("Info: Hello World!");
+    tr_warn("Warn: Hello World!");
+    tr_error("Error: Hello World!");
+
+    while (true)
+    {
+        led = !led;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
+#endif
